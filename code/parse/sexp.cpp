774495bfe7fd6c46b69cabac909894be1ad94328
LuytenKy <luytenky@outlook.com>
2026-04-05 20:48:07 +0300

    remove reduntant return

--- code/parse/sexp.cpp
+++ code/parse/sexp.cpp
@@ -31774,7 +31774,6 @@ int query_operator_return_type(int op)
 		case OP_PLAYER_NOT_USE_AI:
 		case OP_SET_PLAYER_TARGET:
 		case OP_CLEAR_PLAYER_TARGET:
-			return OPR_NULL;
 		case OP_SET_FRIENDLY_DAMAGE_CAPS:
 		case OP_ALLOW_TREASON:
 		case OP_SET_PLAYER_ORDERS:
